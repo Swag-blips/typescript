9df877ea84343c3a7c157f92e8e48062ca6bc286
Swag-blips <coderblip@gmail.com>
2024-10-05 02:11:58 +0100

    arrays&objects

--- sandbox.js
+++ sandbox.js
@@ -9,3 +9,24 @@ var circ = function (diameter) {
 };
 console.log(circ(7.5));
 // arrays
+var names = ["luigi", "mario", "yoshi"];
+names.push("toad");
+var numbers = [10, 20, 30, 40];
+numbers.push(25);
+var mixed = ["ken", 4, "chun-li", 8, 9];
+mixed.push("ryu");
+mixed.push(10);
+mixed[0] = 3;
+// objects
+var ninja = {
+    name: "shaun",
+    belt: "black",
+    age: 30,
+};
+ninja.age = 40;
+ninja.name = "ryu";
+ninja = {
+    name: "yoshi",
+    belt: "orange",
+    age: 40,
+};
--- sandbox.ts
+++ sandbox.ts
@@ -1,4 +1,3 @@
-
 let character = "mario";
 let age = 30;
 let isBlackBelt = false;
@@ -15,3 +14,33 @@ const circ = (diameter: number) => {
 console.log(circ(7.5));
 
 // arrays
+
+let names = ["luigi", "mario", "yoshi"];
+
+names.push("toad");
+
+let numbers = [10, 20, 30, 40];
+numbers.push(25);
+
+let mixed = ["ken", 4, "chun-li", 8, 9];
+
+mixed.push("ryu");
+mixed.push(10);
+mixed[0] = 3;
+
+// objects
+
+let ninja = {
+  name: "shaun",
+  belt: "black",
+  age: 30,
+};
+
+ninja.age = 40;
+ninja.name = "ryu";
+
+ninja = {
+  name: "yoshi",
+  belt: "orange",
+  age: 40,
+};
