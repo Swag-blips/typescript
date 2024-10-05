d2a2bafa775ed5a98cd10fdc1697ce9e3ceefa7c
Swag-blips <coderblip@gmail.com>
2024-10-05 13:03:17 +0100

    functions

--- public/app.js
+++ public/app.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log("testing123");
+// functions
+let greet;
+greet = () => {
+    console.log("Hello world!");
+};
+const add = (a, b, c = 10) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(2, 5);
+const minus = (a, b) => {
+    return a + b;
+};
+let result = minus(10, 7);
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
-console.log("testing123");
+// functions
+
+let greet: Function;
+
+greet = () => {
+  console.log("Hello world!");
+};
+
+const add = (a: number, b: number, c: number | string = 10)  => {
+  console.log(a + b);
+  console.log(c);
+};
+
+add(2, 5);
+
+const minus = (a: number, b: number) => {
+  return a + b;
+};
+
+let result = minus(10, 7);
