317a706f64c40a0849ba54b5bcdb8b37adca8463
Swag-blips <coderblip@gmail.com>
2024-10-05 03:14:13 +0100

    tsconfig and workflows

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,25 +1,26 @@
+"use strict";
 // primitives
-var character = "mario";
-var age = 30;
-var isBlackBelt = false;
+let character = "mario";
+let age = 30;
+let isBlackBelt = false;
 character = "luigi";
 age = 40;
 isBlackBelt = true;
-var circ = function (diameter) {
+const circ = (diameter) => {
     return diameter * Math.PI;
 };
 console.log(circ(7.5));
 // arrays
-var names = ["luigi", "mario", "yoshi"];
+let names = ["luigi", "mario", "yoshi"];
 names.push("toad");
-var numbers = [10, 20, 30, 40];
+let numbers = [10, 20, 30, 40];
 numbers.push(25);
-var mixed = ["ken", 4, "chun-li", 8, 9];
+let mixed = ["ken", 4, "chun-li", 8, 9];
 mixed.push("ryu");
 mixed.push(10);
 mixed[0] = 3;
 // objects
-var ninja = {
+let ninja = {
     name: "shaun",
     belt: "black",
     age: 30,
@@ -33,41 +34,41 @@ ninja = {
 };
 // explicit types
 // explicit primitives
-var character1;
-var age2;
-var isLogged3;
+let character1;
+let age2;
+let isLogged3;
 age = 30;
 isLogged3 = true;
 //explicity arrays
-var ninjas = [];
+let ninjas = [];
 ninjas.push("hello");
 // union types
-var mixed1 = [];
+let mixed1 = [];
 mixed1.push("hello");
 mixed1.push(20);
 mixed1.push(false);
 console.log(mixed);
-var uid;
+let uid;
 uid = 123;
 // objects
-var ninja1;
+let ninja1;
 ninja1 = {
     name: "Hello",
     age: 30,
 };
-var ninja2;
+let ninja2;
 ninja2 = {
     name: "mario",
     age: 20,
     belt: "black",
 };
 // dynamic types
-var age1 = 25;
+let age1 = 25;
 age1 = "hello";
 console.log(age1);
 age1 = { name: "luigi" };
 console.log(age1);
-var mixed2 = [];
+let mixed2 = [];
 mixed2.push(2);
 mixed2.push("Hello");
 mixed2.push(true);
