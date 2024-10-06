dbc63c9e43d4271f71c3a0689590ebc213369930
Swag-blips <coderblip@gmail.com>
2024-10-06 22:21:16 +0100

    generics

---
+++ public/generics.js
@@ -0,0 +1,7 @@
+"use strict";
+// Generics
+const addUID = (obj) => {
+    let uid = Math.floor(Math.random() * 100);
+    return Object.assign(Object.assign({}, obj), { uid });
+};
+let docOne = addUID({ name: "yoshi", age: 40 });
---
+++ src/generics.ts
@@ -0,0 +1,8 @@
+// Generics
+
+const addUID = <T extends object>(obj: T) => {
+  let uid = Math.floor(Math.random() * 100);
+  return { ...obj, uid };
+};
+
+let docOne = addUID({ name: "yoshi", age: 40 });
